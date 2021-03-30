f6760bf18ecbc2eb47109d828511a6da8f863fd6
Manuel S <ManuelDevelopment@users.noreply.github.com>
2021-03-31 01:54:10 +0200

    build: Testing new GitHub action workflow using the db source plugin (#42)

--- plugins/gatsby-source-animethemes-db/src/source-nodes.js
+++ plugins/gatsby-source-animethemes-db/src/source-nodes.js
@@ -16,6 +16,7 @@ module.exports = async ({ actions, createNodeId, createContentDigest, reporter }
 
     const connection = mysql.createConnection({
         host: process.env.DB_HOST || "127.0.0.1",
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE || "animethemes"
