0b07cde9ff6d5111b17fd6ecd6a6975dcf78b25c
Maniload <1999mani@gmail.com>
2021-12-02 03:03:36 +0100

    fix: Fixed order of themes
    * Testing PWA.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
     enabled: process.env.ANALYZE === "true",
 });
+const withPWA = require("next-pwa");
 
-module.exports = withBundleAnalyzer({
+module.exports = withPWA(withBundleAnalyzer({
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
     reactStrictMode: true,
     experimental: {
         styledComponents: true
+    },
+    pwa: {
+        dest: "public"
     }
-});
+}));
