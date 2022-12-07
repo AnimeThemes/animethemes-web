293228519de3f2ef7eeffe54e784f3673e3cece7
ManuelDevelopment <1999mani@gmail.com>
2022-12-07 06:56:47 +0100

    fix: Fixed performance issues with watch history

--- src/hooks/useWatchHistory.ts
+++ src/hooks/useWatchHistory.ts
@@ -1,5 +1,6 @@
 import useLocalStorageState from "use-local-storage-state";
 import type { ThemeSummaryCardThemeFragment } from "generated/graphql";
+import { useCallback } from "react";
 
 interface WatchHistoryTheme extends ThemeSummaryCardThemeFragment {
     id: number
@@ -10,13 +11,13 @@ export type WatchHistory = Array<WatchHistoryTheme>;
 export default function useWatchHistory() {
     const [ history, setHistory ] = useLocalStorageState<WatchHistory>("history", { ssr: true, defaultValue: [] });
 
-    function addToHistory(theme: WatchHistoryTheme) {
-        // Don't add if the most recent entry is the same as the new one
-        if (history[history.length - 1]?.id === theme.id) {
-            return;
-        }
+    const addToHistory = useCallback((theme: WatchHistoryTheme) => {
 
         setHistory((history) => {
+            // Don't add if the most recent entry is the same as the new one
+            if (history[history.length - 1]?.id === theme.id) {
+                return history;
+            }
             // Remove all previous occurences of the theme to avoid duplicates
             const newHistory = history.filter((t) => t.id !== theme.id);
 
@@ -29,7 +30,7 @@ export default function useWatchHistory() {
 
             return newHistory;
         });
-    }
+    }, [setHistory]);
 
     function clearHistory() {
         setHistory([]);
