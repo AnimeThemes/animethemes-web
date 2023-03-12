dd6d6d9a2a60280b2a8c6c914f14774e9783eb31
Mani <12898705+Maniload@users.noreply.github.com>
2023-03-13 00:55:03 +0100

    feat: Various improvements to playlist support (#169)
    * Added action to delete playlists.
    * Added playlist add action to track add dialog.
    * Fixed global search.
    * Fixed busy state of register dialog.
    * Fixed message that shows when no playlist are available.
    * Improved error handling when adding playlists.

--- src/components/card/PlaylistSummaryCard.tsx
+++ src/components/card/PlaylistSummaryCard.tsx
@@ -2,12 +2,38 @@ import { SummaryCard } from "components/card/SummaryCard";
 import type { PropsWithChildren } from "react";
 import gql from "graphql-tag";
 import type { PlaylistSummaryCardPlaylistFragment } from "generated/graphql";
+import type { ReactNode } from "react";
+import styled from "styled-components";
+import theme from "theme";
+
+const StyledWrapper = styled.div`
+    position: relative
+`;
+
+const StyledOverlayButtons = styled.div`
+    position: absolute;
+    right: 16px;
+    opacity: 0;
+    transition-property: opacity;
+
+    ${StyledWrapper}:hover & {
+        position: static;
+        opacity: 1;
+        transition-duration: 250ms;
+    }
+
+    @media (max-width: ${theme.breakpoints.mobileMax}) {
+        position: static;
+        opacity: 1;
+    }
+`;
 
 interface PlaylistSummaryCardProps {
-    playlist: PlaylistSummaryCardPlaylistFragment
+    playlist: PlaylistSummaryCardPlaylistFragment;
+    menu?: ReactNode;
 }
 
-export default function PlaylistSummaryCard({ playlist, children }: PropsWithChildren<PlaylistSummaryCardProps>) {
+export default function PlaylistSummaryCard({ playlist, children, menu, ...props }: PropsWithChildren<PlaylistSummaryCardProps>) {
     const description = (
         <SummaryCard.Description>
             <span>Playlist</span>
@@ -16,9 +42,16 @@ export default function PlaylistSummaryCard({ playlist, children }: PropsWithChi
     );
 
     return (
-        <SummaryCard title={playlist.name} description={description} to={`/playlist/${playlist.id}`}>
-            {children}
-        </SummaryCard>
+        <StyledWrapper>
+            <SummaryCard title={playlist.name} description={description} to={`/playlist/${playlist.id}`} {...props}>
+                {children}
+                {menu ? (
+                    <StyledOverlayButtons onClick={(event) => event.stopPropagation()}>
+                        {menu}
+                    </StyledOverlayButtons>
+                ) : null}
+            </SummaryCard>
+        </StyledWrapper>
     );
 }
 
