b8bfdf35a045490b4e3ea16016c99d50c1cb2db2
ShehabMohsen <aquashehab612@gmail.com>
2023-07-17 22:42:59 -0400

    feat: utilize Framer Motion to animate back to top button

--- src/components/button/BackToTopButton.tsx
+++ src/components/button/BackToTopButton.tsx
@@ -5,16 +5,14 @@ import { faChevronUp } from "@fortawesome/pro-solid-svg-icons";
 import React, { useState, useEffect, useContext } from "react";
 import theme from "theme";
 import { withHover } from "styles/mixins";
-import { slideIn, slideOut } from "styles/animations";
 import PlayerContext from "context/playerContext";
+import { AnimatePresence, m } from "framer-motion";
 
-const ScrollButton = styled(Button)<{ $bottomOffset: number }>`
+const ScrollButton = styled(m(Button))<{ $bottomOffset: number }>`
   position: fixed;
   right: 16px;
   bottom: ${(props) => 16 + props.$bottomOffset}px;
   padding: 16px;
-  display: ${(props) => (props.isVisible ? "default" : "none")};
-  animation: ${(props) => props.animation} 250ms;
 
   ${withHover`
       background-color: ${theme.colors["solid-on-card"]}; 
@@ -24,8 +22,6 @@ const ScrollButton = styled(Button)<{ $bottomOffset: number }>`
 
 export function BackToTopButton() {
   const [isButtonVisible, setIsButtonVisible] = useState(false);
-  const [animation, setAnimation] = useState("slideOut");
-
   const { currentWatchListItem } = useContext(PlayerContext);
 
   function scrollUp() {
@@ -39,34 +35,30 @@ export function BackToTopButton() {
     function handleScroll() {
       if (window.scrollY > 2000) {
         setIsButtonVisible(true);
-        setAnimation("slideIn");
       } else {
-        setAnimation("slideOut");
+        setIsButtonVisible(false);
       }
     }
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-
-  useEffect(() => {
-    // Allow the button to slide out before setting display:none using 'isButtonVisible'
-    if (animation === "slideOut") {
-      setTimeout(() => {
-        setIsButtonVisible(false);
-      }, 200);
-    }
-  }, [animation]);
   return (
-    <ScrollButton
-      variant="primary"
-      isCircle={true}
-      onClick={scrollUp}
-      isVisible={isButtonVisible}
-      animation={animation === "slideIn" ? slideIn() : slideOut("450%", "0%")}
-      onMouseDown={(event: React.PointerEvent) => event.preventDefault()}
-      $bottomOffset={currentWatchListItem ? 76 : 0}
-    >
-      <Icon icon={faChevronUp} />
-    </ScrollButton>
+    <AnimatePresence>
+      {isButtonVisible ? (
+        <ScrollButton
+          key={"scrollButton"}
+          initial={{ y: "150%" }}
+          animate={{ y: "0" }}
+          exit={{ y: "450%" }}
+          variant="primary"
+          isCircle={true}
+          onClick={scrollUp}
+          onMouseDown={(event: React.PointerEvent) => event.preventDefault()}
+          $bottomOffset={currentWatchListItem ? 76 : 0}
+        >
+          <Icon icon={faChevronUp} />
+        </ScrollButton>
+      ) : null}
+    </AnimatePresence>
   );
 }
